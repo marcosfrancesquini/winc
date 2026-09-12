eadb1b5cdb5df023343a7b5f5ac93c69ebe1efcd
marcosfrancesquini <francesquini.personal@gmail.com>
2026-09-12 11:29:18 +0200

    testes

---
+++ testing/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,9 @@
+module.exports = fizzbuzz = (n) => {
+  if (typeof n !== "number") {
+    throw new Error('fizzbuzz needs a number!')
+  }
+  if (n % 15 === 0) return "FizzBuzz";
+  if (n % 5 === 0) return "Buzz";
+  if (n % 3 === 0) return "Fizz";
+  return n
+}
