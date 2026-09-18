c399a6c497fa37872f71c136c91c3f3639f5743b
marcosfrancesquini <francesquini.personal@gmail.com>
2026-09-18 10:52:05 +0200

    first funcion ok

--- finaljsadavanced/index.js
+++ finaljsadavanced/index.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const prompt = require('prompt-sync')();
 const cakeRecipes = require("./cake-recipes.json");
 
 // Your functions here
 
+const showAllAuthors = (cakeRecipes) => {
+  const authorsList = [];
+
+  cakeRecipes.forEach((cakeRecipe) => {
+    if (!authorsList.includes(cakeRecipe.Author)) {
+      authorsList.push(cakeRecipe.Author);
+    }
+  });
+  return authorsList.sort();
+};
 
 
 
@@ -11,14 +22,12 @@ const cakeRecipes = require("./cake-recipes.json");
 
 
 
-
-
-
-
+console.clear();
 
 // Part 2
 
 const displayMenu = () => {
+
   console.log("\nRecipe Management System Menu:");
   console.log("1. Show All Authors");
   console.log("2. Show Recipe names by Author");
@@ -28,16 +37,19 @@ const displayMenu = () => {
   console.log("0. Exit");
   const choice = prompt("Enter a number (1-5) or 0 to exit: ");
   return parseInt(choice);
+
 }
 
 
 let choice;
 
 do {
+
   choice = displayMenu();
 
   switch (choice) {
     case 1:
+      console.log(showAllAuthors(cakeRecipes));
 
       break;
     case 2:
@@ -56,6 +68,7 @@ do {
       console.log("Exiting...");
       break;
     default:
+      console.clear();
       console.log("Invalid input. Please enter a number between 0 and 5.");
   }
 } while (choice !== 0);
